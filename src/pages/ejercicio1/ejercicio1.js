c53668af11754b1dd195821e052984bba9867f03
kalocesa <kalocesa95@gmail.com>
2025-02-06 22:41:01 -0600

    Feat: new functions on exercise

--- src/pages/ejercicio1/ejercicio1.js
+++ src/pages/ejercicio1/ejercicio1.js
@@ -1,1 +1,22 @@
+const firstExcercise = () => {
+  const result = document.getElementById("operacion");
+  result.innerText = `
+    let arr = ["This", "is", "a", "sentence."];
 
+function printOutString() {
+
+}
+
+printOutString();
+    `;
+};
+
+firstExcercise();
+
+let arr = ["This", "is", "a", "sentence."];
+
+function printOutString() {
+  return arr.join(" ");
+}
+
+console.log(printOutString());
